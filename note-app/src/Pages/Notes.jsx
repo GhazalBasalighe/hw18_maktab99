49d89e25fd36bed3a84cccf7751db9bf52afbceb
Ghazal Basalighe <ghazalbasaligheh@gmail.com>
2023-11-01 18:14:25 +0330

    [fix] Proper message after deletion of all notes

--- note-app/src/Pages/Notes.jsx
+++ note-app/src/Pages/Notes.jsx
@@ -4,6 +4,7 @@ function Notes({ notes, handleRedirectAdd, handleRedirectRead }) {
   return (
     <div>
       <MainHeader />
+      {/* IF THERE ARE NOTES */}
       <div className="grid grid-cols-1 my-10 gap-4">
         {notes.map((note) => (
           <div
@@ -15,6 +16,21 @@ function Notes({ notes, handleRedirectAdd, handleRedirectRead }) {
           </div>
         ))}
       </div>
+      {/* IF THERE ARE NO NOTES */}
+      {notes.length === 0 && (
+        <div className="h-screen grid place-items-center">
+          <div>
+            <img
+              src="src/assets/FirstNote.png"
+              alt="add your first note"
+            />
+            <p className="text-xl font-light text-center">
+              Create your first note !
+            </p>
+          </div>
+        </div>
+        //  {/* THE METHOD ABOVE HAS BETTER READABILITY SINCE IT DOESN'T RENDER THE LANDING PAGE AGAIN IF THERE WERE NO NOTES.INSTEAD,SAME PAGE,SAME FOOTER AND HEADER,ONLY THE CONTENT CHANGES 😁*/}
+      )}
       <Footer handleRedirectAdd={handleRedirectAdd} />
     </div>
   );
