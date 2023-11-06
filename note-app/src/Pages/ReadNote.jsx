63751e7d56a76bc9b2f0c747a7c298b196d0241c
Ghazal Basalighe <ghazalbasaligheh@gmail.com>
2023-11-06 14:24:12 +0330

    [fix] Long title and description truncate handling

--- note-app/src/Pages/ReadNote.jsx
+++ note-app/src/Pages/ReadNote.jsx
@@ -2,6 +2,22 @@ import SecondaryHeader from "../Parts/SecondaryHeader";
 function ReadNote(props) {
   const { pageToRender, selectedNote, handleRedirectNotes, deleteNote } =
     props;
+  let component = <h1>Note Not Found</h1>;
+  if (!!selectedNote) {
+    component = (
+      <>
+        <h1 className="my-10 text-5xl break-words overflow-auto">
+          {selectedNote.title}
+        </h1>
+        <textarea
+          readOnly
+          className="text-2xl inputs resize-none overflow-auto"
+        >
+          {selectedNote.description}
+        </textarea>
+      </>
+    );
+  }
   return (
     <div>
       <SecondaryHeader
@@ -10,8 +26,7 @@ function ReadNote(props) {
         deleteNote={deleteNote}
         selectedNote={selectedNote}
       />
-      <h1 className="my-10 text-5xl">{selectedNote.title}</h1>
-      <p className="text-2xl">{selectedNote.description}</p>
+      {component}
     </div>
   );
 }
--- note-app/src/Parts/NoteGenerator.jsx
+++ note-app/src/Parts/NoteGenerator.jsx
@@ -7,8 +7,10 @@ function NoteGenerator({ notes, handleRedirectRead }) {
           key={note.id}
           className={`cursor-pointer rounded-lg p-6 ${note.bgColor} text-black flex flex-col gap-2`}
         >
-          <h3 className="text-lg font-bold">{note.title}</h3>
-          <p className="text-xs">{note.description}</p>
+          <h3 className="text-lg font-bold truncate">{note.title}</h3>
+          <p className="text-xs truncate">
+            {note.description.split("\n")[0]}
+          </p>
         </div>
       ))}
     </div>
